9f606c8e8de8a86dfed1fd0ef0effef751d73047
Allan Aranzaso <67307011+allanaranzaso@users.noreply.github.com>
2021-08-12 10:58:32 -0700

    push updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import dice from "./dice6.png";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen. Age: {props.age}</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <img src={dice} alt="dice"/>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+        <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>Published {props.year} </p>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Truffle pasta",
+  "Spaghetti with meatballz"
+]
+
+const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Cindy" age="2" />
+      <Main adjective="amazing" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
